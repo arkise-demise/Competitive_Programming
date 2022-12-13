f413887c4ce13333373e57cb0e0e0dd242607184
Your Name <you@example.com>
2022-12-14 02:56:30 +0300

    findingTargetIndice

---
+++ targetIndice.py
@@ -0,0 +1,8 @@
+class Solution:
+    def targetIndices(self, nums: List[int], target: int) -> List[int]:
+        result =[]
+        nums.sort()
+        for i in range(len(nums)):
+            if (nums[i]==target):
+                result.append(i)
+        return result    
