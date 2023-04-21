64ad2ea259fb87ac53c6a0d92e6ade120140852e
Your Name <you@example.com>
2023-04-21 17:11:45 +0300

    bubbleSort2 solved

---
+++ bubbleSort2.py
@@ -0,0 +1,15 @@
+list1 = []
+num=int(input("how many numbers you want to enter:"))
+print("Enter values:")
+for k in range(num):
+      list1.append(int(input()))
+print("unsorted list:",list1)
+for j in range(len(list1)-1):
+      for i in range(len(list1)-1-j):
+            if list1[i]>list1[i+1]:
+                  list1[i], list1[i+1]=list1[i+1],list1[i]
+                  print(list1)
+            else:
+                  print(list1)
+      print()
+print("sorted list:",list1)
