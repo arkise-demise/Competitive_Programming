774e5a50533e498419793f1dc3048bd9898a0190
arkise-demise <arkisewdemisew14@gmail.com>
2023-05-09 05:43:28 -0700

    jewels In Stone easy problem solved

---
+++ jewelsInstones.py
@@ -0,0 +1,8 @@
+class Solution:
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        ans=0
+        for i in stones:
+            if i in jewels:
+                ans+=1
+            print(ans)
+        return ans
