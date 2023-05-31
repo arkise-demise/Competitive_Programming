5f8fae85ef47e8b5b7eeeadf83a1194ad657af19
arkise-demise <arkisewdemisew14@gmail.com>
2023-05-31 15:49:06 -0700

    H-index problem solved

---
+++ H-index.py
@@ -0,0 +1,8 @@
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        citations.sort()
+        citations=citations[::-1]
+        for i in range(len(citations)):
+            if citations[i]<=i:
+                return i
+        return len(citations)     
