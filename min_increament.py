79fd57b73e562096f4134c1e976b0b2f7f329684
arkise-demise <arkisewdemisew14@gmail.com>
2023-06-21 15:37:35 -0700

    min_increament for unique solved

---
+++ min_increament.py
@@ -0,0 +1,9 @@
+class Solution:
+    def minIncrementForUnique(self, nums: List[int]) -> int:
+        count = 0
+        nums = sorted(nums)
+        for i in range(1, len(nums)):
+            if nums[i] <= nums[i-1]:
+                count = count + (nums[i-1] - nums[i]) + 1
+                nums[i] = nums[i-1] + 1
+        return count
